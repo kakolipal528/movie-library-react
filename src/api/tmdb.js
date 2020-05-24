a9d2bbe6750c9805597432206ba9f721c0b1dfa5
kakolipal528 <39982574+kakolipal528@users.noreply.github.com>
2020-05-24 20:30:48 +0530

    change

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
-    api_key: process.env.REACT_APP_API,
+    api_key: "975493d51d4b685b9b604f336a5b50ee",
   },
 });
